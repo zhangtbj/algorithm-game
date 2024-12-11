1c12212451e381d8685d7ea30480ad00a351fdb5
zhangtbj <zhangtbj@cn.ibm.com>
2024-12-11 20:50:36 +0800

    Add test

--- list/design-linked-list/README.md
+++ list/design-linked-list/README.md
@@ -1,10 +1,10 @@
-# 203. 移除链表元素
+# 707. 设计链表
 
 ## 题目大意
-删除链表中所有指定值的结点。
+这道题比较简单，设计一个链表，实现相关操作即可。
 
-## 视频题解
-https://www.bilibili.com/video/BV18B4y1s7R9?spm_id_from=333.788.player.switch&vd_source=f881def7ea7cf10e6fa73627efe940dd
+## 视频讲解
+https://www.bilibili.com/video/BV1FU4y1X7WD?spm_id_from=333.788.videopod.sections&vd_source=f881def7ea7cf10e6fa73627efe940dd
 
 ## 解题思路
-按照题意做即可，注意best方法为虚拟头节点的方法，具体可以参考视频题解
+这题有一个地方比较坑，题目中 Note 里面写的数值取值范围是 [1, 1000]，笔者把 0 当做无效值。结果 case 里面出现了 0 是有效值。case 和题意不符。
--- list/design-linked-list/my_code.go
+++ list/design-linked-list/my_code.go
@@ -3,37 +3,136 @@ package main
 import "fmt"
 
 func main() {
-	var nums = []int{1, 2, 6, 3, 4, 5, 6}
-	head := &ListNode{Val: nums[0]}
-	current := head
-
-	for i := 1; i < len(nums); i++ {
-		current.Next = &ListNode{Val: nums[i]}
-		current = current.Next
-	}
-
-	var val = 6
-
-	fmt.Println(removeElementsTest(head, val))
+	list := Constructor()
+	list.AddAtHead(1)
+	//list.AddAtTail(1)
+	//list.AddAtTail(2)
+	list.AddAtTail(3)
+	//list.AddAtHead(1)
+	//fmt.Println(list.Get(0))
+	//fmt.Println(list.Get(1))
+	//fmt.Println(list.Get(2))
+	list.AddAtIndex(1, 2)
+	fmt.Println(list.Get(1))
+	list.DeleteAtIndex(1)
+	fmt.Println(list.Get(1))
+	fmt.Println(list.Get(3))
+	list.DeleteAtIndex(3)
+	list.DeleteAtIndex(0)
+	fmt.Println(list.Get(0))
+	list.DeleteAtIndex(0)
+	fmt.Println(list.Get(0))
+	list.Get(0)
+	list.printLinkedList()
 }
 
-func removeElementsTest(head *ListNode, val int) *ListNode {
-	if head == nil {
-		return nil
-	}
+type MyLinkedList struct {
+	Val  int
+	Next *MyLinkedList
+}
 
-	for head != nil && head.Val == val {
-		head = head.Next
-	}
+func Constructor() MyLinkedList {
+	return MyLinkedList{Val: -1}
+}
 
-	current := head
-	for current != nil && current.Next != nil {
-		if current.Next.Val == val {
-			current.Next = current.Next.Next
+func (this *MyLinkedList) Get(index int) int {
+	current := this
+	for i := 0; current != nil; i++ {
+		if i == index {
+			return current.Val
 		} else {
 			current = current.Next
 		}
 	}
-
-	return head
+	return -1
 }
+
+func (this *MyLinkedList) AddAtHead(val int) {
+	if this.Val == -1 {
+		this.Val = val
+	} else {
+		tmp := &MyLinkedList{Val: this.Val, Next: this.Next}
+		this.Val = val
+		this.Next = tmp
+	}
+}
+
+func (this *MyLinkedList) AddAtTail(val int) {
+	//current := this
+	if this.Val == -1 {
+		this.Val = val
+	} else {
+		newNode := &MyLinkedList{Val: val}
+
+		for this.Next != nil {
+			this = this.Next
+		}
+
+		this.Next = newNode
+	}
+
+}
+
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	if index == 0 {
+		this.AddAtHead(val)
+		return
+	}
+
+	current := this
+	for i := 0; current != nil; i++ {
+		if i == index-1 {
+			break
+		}
+		current = current.Next
+	}
+
+	if current != nil {
+		newNode := &MyLinkedList{Val: val, Next: current.Next}
+		current.Next = newNode
+	}
+}
+
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+	if index == 0 {
+		if this.Next != nil {
+			this.Val = this.Next.Val
+			this.Next = this.Next.Next
+		} else {
+			this.Val = -1
+		}
+		return
+	}
+
+	current := this
+	for i := 0; current != nil; i++ {
+		if i == index-1 {
+			break
+		}
+		current = current.Next
+	}
+
+	if current != nil && current.Next != nil {
+		current.Next = current.Next.Next
+	}
+}
+
+// 打印链表
+func (this *MyLinkedList) printLinkedList() {
+	current := this
+	for current != nil { // 遍历链表
+		fmt.Printf("%d->", current.Val) // 打印节点值
+		current = current.Next          // 切换到下一个节点
+	}
+	fmt.Println("NULL")
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Get(index);
+ * obj.AddAtHead(val);
+ * obj.AddAtTail(val);
+ * obj.AddAtIndex(index,val);
+ * obj.DeleteAtIndex(index);
+ */
