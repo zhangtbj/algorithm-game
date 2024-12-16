ce41b0136cab55076d44fb47ba51cd5ac2a44324
zhangtbj <zhangtbj@cn.ibm.com>
2024-12-16 09:09:34 +0800

    Add test

---
+++ binary-tree/binary-tree-preorder-traversal/README.md
@@ -0,0 +1,10 @@
+# 144. 二叉树的前序遍历
+
+## 题目大意
+先根遍历一颗树。
+
+## 视频讲解
+https://www.bilibili.com/video/BV1Wh411S7xt?spm_id_from=333.788.player.switch&vd_source=f881def7ea7cf10e6fa73627efe940dd
+
+## 解题思路
+两种递归的实现方法，见代码。
---
+++ binary-tree/binary-tree-preorder-traversal/my_code.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BuildTreeFromArray(arr []interface{}) *TreeNode {
+	if len(arr) == 0 {
+		return nil
+	}
+
+	// 将根节点初始化
+	root := &TreeNode{Val: arr[0].(int)}
+	queue := []*TreeNode{root} // 用队列来存储当前层的节点
+	index := 1                 // 从数组的第二个元素开始处理
+
+	for index < len(arr) {
+		// 取队列头节点作为当前父节点
+		current := queue[0]
+		queue = queue[1:]
+
+		// 处理左子节点
+		if arr[index] != nil {
+			leftNode := &TreeNode{Val: arr[index].(int)}
+			current.Left = leftNode
+			queue = append(queue, leftNode)
+		}
+		index++
+
+		// 处理右子节点
+		if index < len(arr) && arr[index] != nil {
+			rightNode := &TreeNode{Val: arr[index].(int)}
+			current.Right = rightNode
+			queue = append(queue, rightNode)
+		}
+		index++
+	}
+
+	return root
+}
+
+func printTree(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.Val)
+	printTree(root.Left)
+	printTree(root.Right)
+}
+
+func main() {
+	arr := []interface{}{1, nil, 2, 3}
+	root := BuildTreeFromArray(arr)
+	//fmt.Println("Build binary-tree:")
+
+	result := preorderTraversal(root)
+
+	fmt.Println(result)
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var test []int
+	if root == nil {
+		return test
+	}
+
+	test = append(test, root.Val)
+	test = append(test, preorderTraversal(root.Left)...)
+	test = append(test, preorderTraversal(root.Right)...)
+	return test
+}
